4f0c498640d5e7e4ad829184f17e2aaa7154e8d9
Sasank Maganti <96193563+sasgit-hub@users.noreply.github.com>
2026-03-23 17:27:32 -0500

    user

--- lookup-service-server/app/main.py
+++ lookup-service-server/app/main.py
@@ -20,25 +20,25 @@ logging.basicConfig(level=logging.DEBUG)
 
 
 # Acquire a tracer
-trace.set_tracer_provider(TracerProvider())
-tracer = trace.get_tracer(__name__)
+#trace.set_tracer_provider(TracerProvider())
+#tracer = trace.get_tracer(__name__)
 
-if os.getenv('OTEL_COLLECTOR_ENDPOINT'):
-    trace_exporter = OTLPSpanExporter(endpoint="{}/v1/traces".format(os.getenv('OTEL_COLLECTOR_ENDPOINT')),
-                                      certificate_file=os.getenv('OTEL_COLLECTOR_CERT'), 
-                                      client_key_file=os.getenv('OTEL_COLLECTOR_CLIENT_KEY'), 
-                                      client_certificate_file=os.getenv('OTEL_COLLECTOR_CLIENT_CERT'), 
-                                      headers=os.getenv('OTEL_COLLECTOR_HEADERS'), 
-                                      timeout=os.getenv('OTEL_COLLECTOR_TIMEOUT'), 
-                                      compression=os.getenv('OTEL_COLLECTOR_COMPRESSION'), 
-                                      session=os.getenv('OTEL_COLLECTOR_SESSION')
-                                      )
-else:
-    trace_exporter = ConsoleSpanExporter()
-trace.get_tracer_provider().add_span_processor(
-    BatchSpanProcessor(trace_exporter))
-FastAPIInstrumentor().instrument_app(app)
-ElasticsearchInstrumentor().instrument()
+#if os.getenv('OTEL_COLLECTOR_ENDPOINT'):
+#    trace_exporter = OTLPSpanExporter(endpoint="{}/v1/traces".format(os.getenv('OTEL_COLLECTOR_ENDPOINT')),
+#                                      certificate_file=os.getenv('OTEL_COLLECTOR_CERT'), 
+#                                      client_key_file=os.getenv('OTEL_COLLECTOR_CLIENT_KEY'), 
+#                                      client_certificate_file=os.getenv('OTEL_COLLECTOR_CLIENT_CERT'), 
+#                                      headers=os.getenv('OTEL_COLLECTOR_HEADERS'), 
+#                                      timeout=os.getenv('OTEL_COLLECTOR_TIMEOUT'), 
+#                                      compression=os.getenv('OTEL_COLLECTOR_COMPRESSION'), 
+#                                      session=os.getenv('OTEL_COLLECTOR_SESSION')
+#                                      )
+#else:
+#    trace_exporter = ConsoleSpanExporter()
+#trace.get_tracer_provider().add_span_processor(
+#    BatchSpanProcessor(trace_exporter))
+#FastAPIInstrumentor().instrument_app(app)
+#ElasticsearchInstrumentor().instrument()
 
 app.include_router(records.router)
 app.include_router(backward_record.router)
